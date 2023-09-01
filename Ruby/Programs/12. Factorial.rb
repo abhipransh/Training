0fa466d98a331897074dafaefc0b8271e478ee47
abhipransh <abhipranshpratap1@gmail.com>
2023-09-01 17:54:57 +0530

    modified program 12

--- Ruby/Programs/12. Factorial.rb
+++ Ruby/Programs/12. Factorial.rb
@@ -1,16 +1,18 @@
+def factorial(num)
+	fact = 1
+
+	if (num != 0)
+		i=1
+		while(i <= num)
+			fact = fact*i
+			i += 1
+		end
+	end
+
+	return fact
+end
+
 puts "Enter the number to find the factorial:"
 num = gets.chomp.to_i
 
-fact = 1
-
-if (num == 0)
-	puts "Error! Could not find the factorial"
-else
-	i=1
-	while(i <= num)
-		fact = fact*i
-		i += 1
-	end
-end
-		
-puts "factorial of #{num} is #{fact}"
+puts "Factorial of #{num} is #{factorial(num)}"
