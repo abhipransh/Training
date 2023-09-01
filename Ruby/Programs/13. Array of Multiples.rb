4cc7cfaa66992dbba0b30dd57d4b386c1da83e66
abhipransh <abhipranshpratap1@gmail.com>
2023-09-01 17:41:14 +0530

    added program 13

--- Ruby/Programs/13. Array of Multiples.rb
+++ Ruby/Programs/13. Array of Multiples.rb
@@ -3,21 +3,20 @@
 #array_of_multiples(12, 10) ➞ [12, 24, 36, 48, 60, 72, 84, 96, 108, 120]
 #array_of_multiples(17, 6) ➞ [17, 34, 51, 68, 85, 102]
 
+def array_of_multiples (num, length)
+    nums = Array.new(length)
+	i = 1
+    while i <= length
+        nums[i-1] = num*i
+        i += 1
+    end
+    return nums
+end
+
 puts "Enter a number:"
 num = gets.chomp.to_i
 
 puts "Enter the length:"
 length = gets.chomp.to_i
 
-def array_of_multiples(num, length) 
-    arr = Array.new
-    i = 1
-    while i <= 5
-        arr.push(num*i)
-        i =+ 1
-    end
-end
-
-array_of_multiples(num, length)
-
-arr.each {|n| puts "The number is: #{n}"}
+puts "#{array_of_multiples(num, length)}"
